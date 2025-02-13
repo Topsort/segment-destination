ca11bc1279d6c2bc7a91997496d2b439a783db11
agustinespildora <jaespildora@uc.cl>
2025-02-13 11:35:52 -0300

    feat: add test for vendorId

--- packages/destination-actions/src/destinations/topsort/purchase/__tests__/index.test.ts
+++ packages/destination-actions/src/destinations/topsort/purchase/__tests__/index.test.ts
@@ -6,7 +6,49 @@ import Destination from '../../index'
 const testDestination = createTestIntegration(Destination)
 
 describe('Topsort.purchase', () => {
-  it('should be successful with default mappings and products object, including vendorId', async () => {
+  it('should be successful with default mappings and products object', async () => {
+    nock(/.*/).persist().post(/.*/).reply(200)
+
+    const event = createTestEvent({
+      products: [
+        {
+          product_id: '123',
+          price: 100,
+          quantity: 1
+        }
+      ]
+    })
+
+    const responses = await testDestination.testAction('purchase', {
+      event,
+      settings: {
+        api_key: 'bar'
+      },
+      useDefaultMappings: true
+    })
+
+    expect(responses.length).toBe(1)
+    expect(responses[0].status).toBe(200)
+    expect(responses[0].options.headers).toMatchSnapshot()
+    expect(responses[0].options.json).toMatchObject({
+      purchases: expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(String),
+          occurredAt: expect.any(String),
+          opaqueUserId: expect.any(String),
+          items: [
+            {
+              productId: '123',
+              unitPrice: 100,
+              quantity: 1
+            }
+          ]
+        })
+      ])
+    })
+  })
+
+  it('should be successful with default mappings and products object, including brand as vendorId', async () => {
     nock(/.*/).persist().post(/.*/).reply(200)
 
     const event = createTestEvent({
@@ -16,6 +58,50 @@ describe('Topsort.purchase', () => {
           price: 100,
           quantity: 1,
           brand: 'v123'
+        }
+      ]
+    })
+
+    const responses = await testDestination.testAction('purchase', {
+      event,
+      settings: {
+        api_key: 'bar'
+      },
+      useDefaultMappings: true
+    })
+
+    expect(responses.length).toBe(1)
+    expect(responses[0].status).toBe(200)
+    expect(responses[0].options.headers).toMatchSnapshot()
+    expect(responses[0].options.json).toMatchObject({
+      purchases: expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(String),
+          occurredAt: expect.any(String),
+          opaqueUserId: expect.any(String),
+          items: [
+            {
+              productId: '123',
+              unitPrice: 100,
+              quantity: 1,
+              vendorId: 'v123'
+            }
+          ]
+        })
+      ])
+    })
+  })
+
+  it('should be successful with default mappings and products object, including custom vendorId', async () => {
+    nock(/.*/).persist().post(/.*/).reply(200)
+
+    const event = createTestEvent({
+      products: [
+        {
+          product_id: '123',
+          price: 100,
+          quantity: 1,
+          vendorId: 'v123'
         }
       ]
     })
